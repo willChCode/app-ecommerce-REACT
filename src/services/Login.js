c5bb5f0612305cde6e595813a6f91093710b37a3
kev <kevbs.jsx@gmail.com>
2022-11-16 23:50:41 -0500

    Axios set up

--- src/services/Login.js
+++ src/services/Login.js
@@ -1,18 +1,23 @@
 import axios from 'axios'
-import Cookies from 'universal-cookie'
-const cookies = new Cookies()
+// import Cookies from 'universal-cookie'
+// const cookies = new Cookies()
 
-const baseUrl = 'http://localhost:3000/api/login'
+const baseURL = 'http://localhost:3000/api/login'
 
 const loginPost = async datos => {
-  const { data } = await axios.post(baseUrl, datos)
-  // document.cookie = `token=${data.token}`
-  cookies.set('token', data.token, {
-    path: '/',
-    secure: true,
-    maxAge: 60 * 60 * 24 * 7
+  // const { data } = await axios.post(baseUrl, datos)
+  const { data } = await axios({
+    baseURL,
+    withCredentials: true,
+    method: 'POST',
+    data: datos,
   })
-  console.log(cookies.get('token'))
+  // document.cookie = `token=${data.token}`
+  // cookies.set('token', data.token, { //! Esto ya no hace falta, el backend es el q crea la cookie y la envia al cliente, no el front end
+  //   path: '/',
+  //   secure: true,
+  //   maxAge: 60 * 60 * 24 * 7
+  // })
   return data
 }
 
